602af934b2df45ecb383f29cab131c529f4dc7ea
Kasia <38132455+KatBlasz@users.noreply.github.com>
2023-04-11 22:35:03 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,8 +3,9 @@
 #
 # class LoginPage(BasePage):
 #     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
+#     password_field_xpath ="//*[@name='password']"
+#     sign_in_button_xpath ="//*[contains(@class, 'MuiButton-root')]"
+#     change_language_button_xpath = "//*[text()='English']"
 #
 #     def type_in_email(self, email):
 #         self.field_send_keys(self.login_field_xpath, email)
