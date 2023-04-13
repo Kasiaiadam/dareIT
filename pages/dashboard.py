3c938a94e4674f6be9aa5d804e4ae40fccf22012
Kasia <38132455+KatBlasz@users.noreply.github.com>
2023-04-13 20:52:10 +0200

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -8,8 +8,8 @@ class Dashboard(BasePage):
     sign_out="//*[@id='__next']/div[1]/div/div/div/ul[2]/div[2]"
     choose_language="//*[@id='__next']/div[1]/div/div/div/ul[2]/div[1]"
     players="//*[@id='__next']/div[1]/div/div/div/ul[1]/div[2]"
-    go_to_last_created_player="//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[1]"
-    go_to_last_updated_player ="//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[2]"
-    go_to_last_created_match ="//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[3]"
-    go_to_last_updated_report="//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[5]"
+    go_to_last_created_player="//div[3]/div/div/a[1]"
+    go_to_last_updated_player ="//div/a[2]"
+    go_to_last_created_match ="//div/a[3]"
+    go_to_last_updated_report="//div/a[5]]"
 pass
